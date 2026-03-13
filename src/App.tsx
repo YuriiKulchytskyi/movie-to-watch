0bc4ead5b25ca1315d3eacb57189e72cb30f6424
YuriiKulchytskyi <yurii.kulchytskyi1@gmail.com>
2026-03-13 17:25:13 +0200

    firebase datat transfered to .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import "./App.css";
-// import { GenreList } from "./components/GanreList/GanreList";
-// import { Layout } from "./pages/Layout";
-// import { Route, Routes } from "react-router-dom";
-// import { MoviePage } from "./pages/MoviePage";
-// import { GenrePage } from "./pages/GenrePage";
-// import { SearchPage } from "./pages/SearchPage";
-// import { HomePage } from "./pages/HomePage";
+
 
 function App() {
 
--- src/firebase.ts
+++ src/firebase.ts
@@ -3,16 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-84yWsT8KiiMpUWF-oO5BkOZhahMcYp4",
-  authDomain: "movie-to-watch-161f0.firebaseapp.com",
-  projectId: "movie-to-watch-161f0",
-  storageBucket: "movie-to-watch-161f0.firebasestorage.app",
-  messagingSenderId: "804069949342",
-  appId: "1:804069949342:web:1eb99158eb47599b9ea08f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 export const db = getFirestore(app);
